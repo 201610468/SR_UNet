c4f7d16f5d90b492ec836379c7345585d5b48fff
jinyeong.Park <91312489+201610468@users.noreply.github.com>
2024-06-21 15:08:42 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ def main():
     args.data_range = '1-1600/1-3'
     args.dir_lr = '/home/jbgpl/Downloads/SR_denoising/SeismicSuperResolution/data/field'
     args.apply_field_data = True
-    args.pre_train = '/home/jbgpl/Downloads/SR_denoising/SeismicSuperResolution/experiment/ESRT_paper/model/model_best.pt'
+    args.pre_train = '/home/jbgpl/Downloads/SR_denoising/SeismicSuperResolution/experiment/alpha6/model/model_best.pt'
     checkpoint = utility.checkpoint(args)
     loader = data.Data(args)
     
